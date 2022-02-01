c5a62762620ee1394ffe8614b8573168dd2c115c
TobinWilson <tobinwilson@protonmail.com>
2022-02-01 11:47:17 -0600

    Create EnemyObjectRocket.java

---
+++ factory-pattern/EnemyObjectRocket.java
@@ -0,0 +1,34 @@
+class EnemyObjectRocket implements EnemyObject {
+    private String name;
+    private Integer damage;
+    
+    public EnemyObjectRocket() {
+        setName("ROCKET");
+        setDamage(20);
+    }
+    
+    public void followPlayer() {
+        System.out.println(getName() + " is following you");
+    } 
+    
+    public void scan() {
+        System.out.println(getName() + " is nearby");
+    }
+    
+    public void attack() {
+        System.out.println(getName() + " attacked you for " + getDamage() + " damage");
+    }
+    
+    public Integer getDamage() {
+        return this.damage;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setDamage(Integer damage) {
+        this.damage = damage;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
