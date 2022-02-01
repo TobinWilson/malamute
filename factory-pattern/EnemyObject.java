cb51aef772fcfe15cd46a967c52cc7000fbced6d
TobinWilson <tobinwilson@protonmail.com>
2022-02-01 11:46:17 -0600

    Create EnemyObject.java

---
+++ factory-pattern/EnemyObject.java
@@ -0,0 +1,10 @@
+interface EnemyObject {
+    void scan();
+    void attack();
+    void followPlayer();
+    
+    String getName();
+    Integer getDamage();
+    void setDamage(Integer damage);
+    void setName(String name);
+}
